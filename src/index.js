a48b3da6ad0aac3bd5b1b3e331cf8eb31d571b2b
Huynh Chi <huynhchi90524@gmail.com>
2023-09-02 15:05:04 +0700

    xong phan 2 - kien thuc cot loi node js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,52 @@
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const path = require('path') 
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // config static files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+// middleware
+app.use(
+   express.urlencoded({
+      extended: true,
+   }),
+); // dùng cho html
+app.use(express.json()); // dùng cho XML, các thư viện axios, fetch (gửi từ code JS)
 
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Teplate engine handlebars
-app.engine('.hbs', handlebars.engine({
-  extname: '.hbs'
-}));
+app.engine(
+   '.hbs',
+   handlebars.engine({
+      extname: '.hbs',
+   }),
+);
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
-  res.render('home');
-})
+   res.render('home');
+});
 
 app.get('/news', (req, res) => {
-  res.render('news');
-})
+   res.render('news');
+});
+
+app.get('/search', (req, res) => {
+   res.render('search');
+});
+
+app.post('/search', (req, res) => {
+   console.log(req.body);
+   res.send('');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+   console.log(`Example app listening on port ${port}`);
+});
